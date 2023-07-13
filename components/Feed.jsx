dbcee9b69507f1b507b6046ecf1f505d9feaf93f
Tamilore <tamiloreolaniyi@gmail.com>
2023-07-13 04:15:43 +0100

    updates

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PrompCardList = ({ data, hangleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          hangleTagClick={hangleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+  };
 
-export default Feed
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="search for a tag or a username"
+          value={searchText}
+          required
+          onChange={handleSearchChange}
+          className="search_input peer"
+        />
+      </form>
+      <PrompCardList data={posts} hangleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
---
+++ components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PromptCard from './PromptCard'
+
+const Profile = ({name,desc,data, handleEdit, handleDelete}) => {
+  return (
+    <section className='w-full'>
+      <h1 className='head_text text-left'>{name} Profile</h1>
+    </section>
+  )
+}
+
+export default Profile
